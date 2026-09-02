ebc1e4ec71f192cc37d2fadd0e32260e1500b000
Samar Hirau <125361431+samarhirau@users.noreply.github.com>
2026-09-02 19:46:21 +0530

    Update Next.js configuration with output and basePath

--- CPP MASTER/next.config.ts
+++ CPP MASTER/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/CPP-Master-',
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
